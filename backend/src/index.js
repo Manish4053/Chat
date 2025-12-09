e0dce152221ba0278718e59e49eb4e2334b69337
Manish4053 <kumarmanishsah445@gmail.com>
2025-12-09 22:54:37 +0530

    last backend pushhh

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,19 +21,19 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if(process.env.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-  });
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+//   });
 
-}
+// }
 
 app.use("/",(req,res)=>{
 res.send("API is running....");  
